90b3a77b06b2ea3d61dfd35c62196c4c1f45c55b
muhammadali057 <wygqkjsaq>
2024-01-29 20:51:45 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+const number1 = 1;
+const number2 = 2;
+const number3 = 3;
+const number4 = 4;
+const number5 = 5;
+const number6 = 6;
+const number7 = 7;
+const number8 = 8;
+const number9 = 9;
+const number10 = 10;
+
+var number11 = 11; 
+var number12 = 12;
+var number13 = 13;
+var number14 = 14;
+var number15 = 15;
+var number16 = 16;
+var number17 = 17;
+var number18 = 18;
+var number19 = 19;
+var number20 = 20;
+
+let number21 = 21;
+let number22 = 22;
+let number23 = 23;
+let number24 = 24;
+let number25 = 25;
+let number26 = 26;
+let number27 = 27;
+let number28 = 28;
+let number29 = 29;
+let number30 = 30;
+
+
+
+confirm("Xushkelibsiz bizning saytimizga")
+prompt("Ismingzni kiriting")
+prompt("Familyangizni kiriting")
+prompt("tug'ilgan sanangizni kiriting")
+prompt("adrisni kiriting")
+prompt("tel nomberni kirteng")
+prompt("yoshingizni kiriting")
+prompt("ishlaysizmi o'qiysizmi")
+alert("tez orada sizbilan bog'lanamiz")
+
